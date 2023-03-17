b2fabb5012ff8fd22fb344f853aaa70216b30e32
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-03-17 15:57:21 +0630

    15-17 April

--- app.js
+++ app.js
@@ -1,20 +1,106 @@
+ 
+const moonid = document.getElementById('moonid')
+const demosection = $('.demosection').get(0)
 
 
-    const moonid = document.getElementById('moonid')
 
-  document.addEventListener('scroll',function(e){
-    const windowscroll = document.documentElement.scrollTop;
-//   const element = document.querySelector('#my-element');
-const rect = moonid.getBoundingClientRect();
+$(document).ready(function(){
 
-if (rect.top < window.innerHeight && rect.bottom >= 0) {
-  // The element is currently visible on the screen.
-  moonid.style.top = "800px"
-} else {
-  // The element is not currently visible on the screen.
-  moonid.style.top = "0px"
-}
+  $(window).scroll(function(){
+
+
+
+    const rect = demosection.getBoundingClientRect();
+    // console.log(rect.top + " rect")
+
+   scrolltop = $(this).scrollTop();
+    // console.log(scrolltop + " scrolltop")
+
+
+    const clientheight = $(moonid).height();
+    // console.log(clientheight + ' client')
+
+    const scrollheight = $(demosection).height();
+    // console.log(scrollheight + ' scroll')
+
+    const curheight = scrollheight - clientheight;
+    // console.log(curheight + ' curheight')
+
+
+
+    const finalheight = (scrolltop  / 10) % curheight 
+    console.log(finalheight + ' final')
+
+      moonid.style.top = `${finalheight}px`
+      moonid.style.transform = "scale(.7) rotate(60px)"
+
+
+  
+
+
+ 
+
   })
+
+
+})
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // get a reference to the animated element
+
+// listen for the scroll event on the window
+// window.addEventListener('scroll', function() {
+//   // get the current scroll position
+//   const scrollPosition = window.scrollY;
+
+//   // get the position of the animated element
+//   const animatedElementPosition = moonid.offsetTop;
+
+//   // calculate the distance between the scroll position and the animated element
+//   const distance = animatedElementPosition - scrollPosition;
+
+//   // check if the distance is less than or equal to the height of the viewport
+//   if (distance <= window.innerHeight) {
+//     // stop the animation
+//     moonid.style.animationPlayState = 'paused';
+//   } else {
+//     // resume the animation
+//     moonid.style.animationPlayState = 'running';
+//   }
+// });
+
+
+
+
+
+
 
 
   
